d08f146df7461e42a8fee397eb7e36ad021730d4
Mariam Elmetwally <mariamfadl2004@gmail.com>
2025-09-04 17:46:56 +0300

    Comment

--- HeaLEOO/Controllers/DoctorsController.cs
+++ HeaLEOO/Controllers/DoctorsController.cs
@@ -10,7 +10,7 @@ namespace HeaLEOO.Controllers
     {
         private readonly IServiceDoctors _doctorService;
         private readonly IClinicsService _clinicsService;
-
+        //Coment...//
         public DoctorsController(IServiceDoctors doctorService, IClinicsService clinicsService)
         {
             _doctorService = doctorService ?? throw new ArgumentNullException(nameof(doctorService));
