ee7f4bdcc203a5254056d01498a1c051cb74cea2
Nada Eslam <nade62204@gmail.com>
2025-09-04 14:00:54 +0300

    services

--- HeaLEOO/DTOs/DoctorsDto.cs
+++ HeaLEOO/DTOs/DoctorsDto.cs
@@ -2,6 +2,7 @@
 {
     public class DoctorsDto
     {
+        //comment..
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string phoneNumber { get; set; } = default!;
