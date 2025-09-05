9843c3b2d959566a27f8b0f13b4f4787a5435de6
Nada Eslam <nade62204@gmail.com>
2025-09-05 22:47:24 +0300

    services part

--- HeaLEOO/ALLServices/IServiceLM.cs
+++ HeaLEOO/ALLServices/IServiceLM.cs
@@ -2,6 +2,10 @@
 {
     public interface IServiceLM
     {
-
+        Task<IEnumerable<ServiceVM>> GetAllAsync();
+        Task<ServiceVM?> GetByIdAsync(int id);
+        Task<ServiceVM> CreateAsync(ServiceVM vm);
+        Task<bool> UpdateAsync(int id, ServiceVM vm);
+        Task<bool> DeleteAsync(int id);
     }
 }
