09a3b4bb1b26138493a1d94197fe0252f6589667
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-05 16:02:42 +0300

    edit3

---
+++ HeaLEOO/Controllers/SpecializationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeaLEOO.Controllers
+{
+    public class SpecializationController : Controller
+    {
+        private readonly IserviceSpecializations _serviceSpecializations;
+
+        public SpecializationController(IserviceSpecializations serviceSpecializations)
+        {
+            _serviceSpecializations = serviceSpecializations;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
