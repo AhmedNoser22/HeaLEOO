e1c089e0a65017a76e14568881431355df07c6c7
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2025-09-06 14:39:39 +0300

    role interface

---
+++ HeaLEOO/ALLServices/IRoleService.cs
@@ -0,0 +1,12 @@
+﻿namespace HeaLEOO.ALLServices
+{
+    public interface IRoleService
+    {
+        
+            IEnumerable<IdentityRole> GetRoles();
+            Task<IdentityRole> CreateRoleAsync(IdentityRole role);
+            Task<IEnumerable<UserManagerVM>> GetRolesAsync(string id);
+            Task<IdentityResult> AddUserToRoleAsync(List<UserManagerVM> managerVMs, string id);
+        
+    }
+}
