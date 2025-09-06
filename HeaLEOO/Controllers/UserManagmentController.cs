86720c0dd54427e01af740d7a1bad6fea519dd66
Mariam Elmetwally <mariamfadl2004@gmail.com>
2025-09-06 15:39:40 +0300

    RoleController

---
+++ HeaLEOO/Controllers/UserManagmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeaLEOO.Controllers
+{
+    public class UserManagmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
