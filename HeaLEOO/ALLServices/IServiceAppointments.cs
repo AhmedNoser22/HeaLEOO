a25045a2b284cfd0f2a3183e4a332bcfabc714a0
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-07 17:14:26 +0300

    Editing Appointments Service

---
+++ HeaLEOO/ALLServices/IServiceAppointments.cs
@@ -0,0 +1,10 @@
+﻿namespace HeaLEOO.ALLServices
+{
+    public interface IServiceAppointments
+    {
+        Task<IEnumerable<AppointmentsVM>> GetAllItems();
+        Task<AppointmentsVM> GetItemById(int id);
+        Task<bool> CreateItem(AppointmentsVM model);
+        Task<bool> DeletItem(int id);
+    }
+}
---
+++ HeaLEOO/ALLServices/ServiceAppointments.cs
@@ -0,0 +1,29 @@
+﻿
+namespace HeaLEOO.ALLServices
+{
+    public class ServiceAppointments : IServiceAppointments
+    {
+        private readonly IGenericRepo<Appointments> _genericRepo;
+        private readonly IMapper _mapper;
+
+        public Task<bool> CreateItem(AppointmentsVM model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AppointmentsVM>> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppointmentsVM> GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
