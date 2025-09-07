17628865322dc49e17326cbb8ce3a1bf670c6286
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-07 15:03:42 +0300

    Editing UserManagement

---
+++ HeaLEOO/ALLServices/IServiceUser_Management.cs
@@ -0,0 +1,9 @@
+﻿namespace HeaLEOO.ALLServices
+{
+    public interface IServiceUser_Management
+    {
+        Task<List<AppUserDto>> GetAllUsersAsync();
+        Task<AppUserDto?> GetUserByIdAsync(string userId);
+        Task<bool> DeleteUserAsync(string userId);
+    }
+}
---
+++ HeaLEOO/ALLServices/ServiceUser_Management.cs
@@ -0,0 +1,30 @@
+﻿
+namespace HeaLEOO.ALLServices
+{
+    public class ServiceUser_Management : IServiceUser_Management
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public ServiceUser_Management(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public Task<bool> DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUserDto>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUserDto?> GetUserByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
