d2056d85a0b1c053ef51335ed88b332b695167fd
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-07 17:41:57 +0300

    Specialization Controller Edit

---
+++ HeaLEOO/Controllers/SpecializationsController.cs
@@ -0,0 +1,18 @@
+﻿using HeaLEOO.ALLServices;
+using HeaLEOO.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HeaLEOO.Controllers
+{
+    public class SpecializationsController : Controller
+    {
+        private readonly IServiceSpec _serviceSpecializations;
+
+        public SpecializationsController(IServiceSpec serviceSpecializations)
+        {
+            _serviceSpecializations = serviceSpecializations;
+        }
+
+    }
+}
