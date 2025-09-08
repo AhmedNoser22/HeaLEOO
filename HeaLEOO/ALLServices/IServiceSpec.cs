3567e7c547861bf8f0325677a4fa92952967a872
Nada Eslam <nade62204@gmail.com>
2025-09-09 01:59:04 +0300

    get specilization method

--- HeaLEOO/ALLServices/IServiceSpec.cs
+++ HeaLEOO/ALLServices/IServiceSpec.cs
@@ -2,5 +2,7 @@
 {
     public interface IServiceSpec
     {
+        Task<IEnumerable<SpecializationsVM>> GetSpecializations();
+        
     }
 }
--- HeaLEOO/ALLServices/ServiceSpec.cs
+++ HeaLEOO/ALLServices/ServiceSpec.cs
@@ -2,5 +2,17 @@
 {
     public class ServiceSpec: IServiceSpec
     {
+        private readonly IGenericRepo<Specializations> _genericRepo;
+        private readonly IMapper _mapper;
+        public ServiceSpec(IGenericRepo<Specializations> genericRepo, IMapper mapper = null)
+        {
+            _genericRepo = genericRepo;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<SpecializationsVM>> GetSpecializations()
+        {
+            var specializations = await _genericRepo.GetAll();
+            return _mapper.Map<IEnumerable<SpecializationsVM>>(specializations);
+        }
     }
 }
