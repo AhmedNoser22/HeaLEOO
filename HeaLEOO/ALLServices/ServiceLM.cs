196e99b5657856e83fe5c5e9344f012319ace330
fatma <hegazyfatma23@gmail.com>
2025-09-08 10:47:30 +0300

    add service

---
+++ HeaLEOO/ALLServices/ServiceLM.cs
@@ -0,0 +1,29 @@
+﻿namespace HeaLEOO.ALLServices
+{
+    public class ServiceLM : IServiceLM
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceLM(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ServiceVM>> GetAllAsync()
+        {
+            return await _context.Services
+                     .Select(s => new ServiceVM
+                     {
+                         Name = s.Name,
+                         Price = s.Price
+                     })
+                     .ToListAsync();
+        }
+
+
+        
+
+       
+       
+    }
+}
