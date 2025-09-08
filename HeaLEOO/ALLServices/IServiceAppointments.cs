6ee32fa2e4f85becdb2967cbe1ddde5454aaa2e0
Nada Eslam <nade62204@gmail.com>
2025-09-08 21:32:52 +0300

    mapper & dependancy injection

--- HeaLEOO/ALLServices/IServiceAppointments.cs
+++ HeaLEOO/ALLServices/IServiceAppointments.cs
@@ -2,9 +2,6 @@
 {
     public interface IServiceAppointments
     {
-        Task<IEnumerable<AppointmentsVM>> GetAllItems();
-        Task<AppointmentsVM> GetItemById(int id);
-        Task<AppointmentsVM> CreateItem(AppointmentsVM model);
-        Task<bool> DeletItem(int id);
+ 
     }
 }
