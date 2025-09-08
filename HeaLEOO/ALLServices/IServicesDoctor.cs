6cd24e397a30127db381f7562e1e06f291a4b445
Nada Eslam <nade62204@gmail.com>
2025-09-09 02:17:49 +0300

    get all doctors method

--- HeaLEOO/ALLServices/IServicesDoctor.cs
+++ HeaLEOO/ALLServices/IServicesDoctor.cs
@@ -2,5 +2,6 @@
 {
     public interface IServicesDoctor
     {
+        Task<IEnumerable<DoctorViewModel>> GetAllItems();
     }
 }
--- HeaLEOO/ALLServices/ServicesDoctor.cs
+++ HeaLEOO/ALLServices/ServicesDoctor.cs
@@ -2,5 +2,31 @@
 {
     public class ServicesDoctor: IServicesDoctor
     {
+        private readonly IGenericRepo<Doctors> _genericRepo;
+        private readonly IMapper _mapper;
+        private readonly IserviceSpecializations _serviceSpecializations;
+        private readonly IServiceClinDate _serviceClinDate;
+
+        public ServicesDoctor(
+            IGenericRepo<Doctors> genericRepo,
+            IMapper mapper,
+            IserviceSpecializations serviceSpecializations,
+            IServiceClinDate serviceClinDate)
+        {
+            _genericRepo = genericRepo;
+            _mapper = mapper;
+            _serviceSpecializations = serviceSpecializations;
+            _serviceClinDate = serviceClinDate;
+        }
+
+        public async Task<IEnumerable<DoctorViewModel>> GetAllItems()
+        {
+            var doctors = await _genericRepo.GetAll(query =>
+                query.Include(d => d.specializations)
+                     .Include(d => d.ClinicDoctors)
+                     .ThenInclude(cd => cd.Clinic)
+            );
+            return _mapper.Map<IEnumerable<DoctorViewModel>>(doctors);
+        }
     }
 }
