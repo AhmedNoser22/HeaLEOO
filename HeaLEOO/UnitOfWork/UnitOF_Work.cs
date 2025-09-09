2b20ddb6b021288e4366355c043a10f80166c1d8
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-09 17:13:53 +0300

    add to unit of work

--- HeaLEOO/UnitOfWork/UnitOF_Work.cs
+++ HeaLEOO/UnitOfWork/UnitOF_Work.cs
@@ -1,6 +1,34 @@
 ﻿namespace HeaLEOO.UnitOfWork
 {
-    public class UnitOF_Work
+    public class UnitOF_Work : IUnitOF_Work
     {
+        private readonly AppDbContext _context;
+        public IGenericRepo<Clinics> GetRepoCliinics { get; private set; }
+        public IGenericRepo<Doctors> GetRepoDoctors { get; private set; }
+
+        public IGenericRepo<Appointments> GetRepoAppointments { get; private set; }
+
+        public IGenericRepo<ModelService> GetRepoModelService { get; private set; }
+
+        public IGenericRepo<ServiceSpec> GetRepoServiceSpec { get; private set; }
+        public UnitOF_Work(AppDbContext context)
+        {
+            _context = context;
+            GetRepoCliinics = new GenericRepo<Clinics>(_context);
+            GetRepoDoctors = new GenericRepo<Doctors>(_context);
+            GetRepoAppointments = new GenericRepo<Appointments>(_context);
+            GetRepoModelService = new GenericRepo<ModelService>(_context);
+            GetRepoServiceSpec = new GenericRepo<ServiceSpec>(_context);
+        }
+
+        public async Task<bool> Complete()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
