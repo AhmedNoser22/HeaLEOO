0df34727ab1001cec0085d1a5cdf48a897e8a4f4
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-09 16:04:46 +0300

    edit index

--- HeaLEOO/Mapping/MapProfile.cs
+++ HeaLEOO/Mapping/MapProfile.cs
@@ -35,10 +35,9 @@ namespace HeaLEOO.Mapping
                 .ForMember(dest => dest.clinicId, opt => opt.MapFrom(src => src.ClinicId))
                 .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Clinic.Name))
                 .ForMember(dest => dest.Clinics, opt => opt.Ignore());
-
             CreateMap<ServiceVM, ModelService>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.Clinic, opt => opt.Ignore());
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Clinic, opt => opt.Ignore());
             CreateMap<AppUser, AppUserDto>();
             CreateMap<AppUserDto, AppUser>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
