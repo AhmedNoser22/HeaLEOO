fe5ba6b28c9e2bce802479444b34cc52aaa86058
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-11 19:38:59 +0300

    Add Map of DerviceModel

--- HeaLEOO/Mapping/MapProfile.cs
+++ HeaLEOO/Mapping/MapProfile.cs
@@ -9,6 +9,7 @@ namespace HeaLEOO.Mapping
             DoctorMap();
             SpecializationMap();
             ClinicMap();
+            ServiceModelMap();
             CreateMap<RegisterVM,AppUser>()
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
@@ -17,13 +18,6 @@ namespace HeaLEOO.Mapping
             .ForMember(dest => dest.status, opt => opt.MapFrom(src => src.status))
             .ForMember(dest => dest.isActive, opt => opt.MapFrom(src => src.isActive));
             CreateMap<AppointmentsVM, Appointments>();
-            CreateMap<ModelService, ServiceVM>()
-                .ForMember(dest => dest.clinicId, opt => opt.MapFrom(src => src.ClinicId))
-                .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Clinic.Name))
-                .ForMember(dest => dest.Clinics, opt => opt.Ignore());
-            CreateMap<ServiceVM, ModelService>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.Clinic, opt => opt.Ignore());
             CreateMap<AppUser, AppUserDto>();
             CreateMap<AppUserDto, AppUser>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
--- HeaLEOO/Mapping/MethodsMap/ServiceModelMap.cs
+++ HeaLEOO/Mapping/MethodsMap/ServiceModelMap.cs
@@ -2,5 +2,15 @@
 {
     public partial class MapProfile
     {
+        public void ServiceModelMap()
+        {
+            CreateMap<ModelService, ServiceVM>()
+                .ForMember(dest => dest.clinicId, opt => opt.MapFrom(src => src.ClinicId))
+                .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Clinic.Name))
+                .ForMember(dest => dest.Clinics, opt => opt.Ignore());
+            CreateMap<ServiceVM, ModelService>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Clinic, opt => opt.Ignore());
+        }
     }
 }
