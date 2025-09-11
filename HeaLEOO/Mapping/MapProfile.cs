4aac6cabcc956ffbd170f577d1f736afbbf43972
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-11 19:54:45 +0300

    Add Map of UserManagement

--- HeaLEOO/Mapping/MapProfile.cs
+++ HeaLEOO/Mapping/MapProfile.cs
@@ -11,19 +11,12 @@ namespace HeaLEOO.Mapping
             ClinicMap();
             ServiceModelMap();
             AppointmentMap();
+            UserManagementMap();
             CreateMap<RegisterVM,AppUser>()
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
             CreateMap<AppUser, AppUserDto>();
             CreateMap<AppUserDto, AppUser>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
-            CreateMap<AppUser, UserManagerVM>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
-            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
-            CreateMap<UserManagerVM, AppUser>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
--- HeaLEOO/Mapping/MethodsMap/UserManagementMap.cs
+++ HeaLEOO/Mapping/MethodsMap/UserManagementMap.cs
@@ -2,5 +2,16 @@
 {
     public partial class MapProfile
     {
+        public void UserManagementMap()
+        {
+            CreateMap<AppUser, UserManagerVM>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+            CreateMap<UserManagerVM, AppUser>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+        }
     }
 }
