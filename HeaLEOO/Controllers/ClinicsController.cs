78d148fa3433fc924d51a3489a68ba0d076a3c61
Mariam Elmetwally <mariamfadl2004@gmail.com>
2025-09-11 18:02:08 +0300

    ClinicsController

--- HeaLEOO/Controllers/ClinicsController.cs
+++ HeaLEOO/Controllers/ClinicsController.cs
@@ -3,41 +3,4 @@
     private readonly IServiceClinicsDB _serviceClinics;
     private readonly IMapper _mapper;
 
-    public ClinicsController(IServiceClinicsDB serviceClinics, IMapper mapper)
-    {
-        _serviceClinics = serviceClinics;
-        _mapper = mapper;
-    }
-    public async Task<IActionResult> Index()
-    {
-        var clinics = await _serviceClinics.GetAllClinicsAsync();
-        return View(clinics);
-    }
-    public async Task<IActionResult> Details(int id)
-    {
-        var clinic = await _serviceClinics.GetClinicByIdAsync(id);
-        if (clinic == null) return NotFound();
-
-        return View(clinic);
-    }
-    public IActionResult Create()
-    {
-        return View();
-    }
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(ClinicVM model, IFormFile file)
-    {
-        if (!ModelState.IsValid) return View(model);
-
-        await _serviceClinics.AddClinicAsync(model, file);
-        return RedirectToAction(nameof(Index));
-    }
-    public async Task<IActionResult> Delete(int id)
-    {
-        var success = await _serviceClinics.DeleteClinicAsync(id);
-        if (!success) return NotFound();
-
-        return RedirectToAction(nameof(Index));
-    }
 }
