c9d105576fe3733ddccc38f80ddfeaad35159c75
Mariam Elmetwally <mariamfadl2004@gmail.com>
2025-09-11 18:02:51 +0300

    ClinicsController

--- HeaLEOO/Controllers/ClinicsController.cs
+++ HeaLEOO/Controllers/ClinicsController.cs
@@ -3,4 +3,11 @@
     private readonly IServiceClinicsDB _serviceClinics;
     private readonly IMapper _mapper;
 
+    public ClinicsController(IServiceClinicsDB serviceClinics, IMapper mapper)
+    {
+        _serviceClinics = serviceClinics;
+        _mapper = mapper;
+    }
+
+
 }
