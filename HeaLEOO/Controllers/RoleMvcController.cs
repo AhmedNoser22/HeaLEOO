b96fde0b2edeeffc2e167a349012c7855b76af18
Mariam Elmetwally <mariamfadl2004@gmail.com>
2025-09-11 18:23:57 +0300

    RoleMvcController

--- HeaLEOO/Controllers/RoleMvcController.cs
+++ HeaLEOO/Controllers/RoleMvcController.cs
@@ -1,33 +1,4 @@
 ﻿public class RoleMvcController : Controller
 {
-    private readonly IRoleService _roleService;
-
-    public RoleMvcController(IRoleService roleService)
-    {
-        _roleService = roleService;
-    }
-
-    public async Task<IActionResult> Users()
-    {
-        var users = await _roleService.GetAllUsers();
-        return View(users);
-    }
-    public async Task<IActionResult> ManageUserRoles(string userName)
-    {
-        var model = await _roleService.GetUserRoles(userName);
-        ViewBag.UserName = userName;
-        return View(model);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> ManageUserRoles(string userName, List<RoleViewModel> model)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View(model);
-        }
-
-        await _roleService.UpdateUserRoles(userName, model);
-        return RedirectToAction(nameof(Users));
-    }
+    
 }
