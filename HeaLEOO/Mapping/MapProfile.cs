a50067ba64d12f4fcd07ffc710565f02e7099f10
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-11 20:03:21 +0300

    Add Map Of Register

--- HeaLEOO/Mapping/MapProfile.cs
+++ HeaLEOO/Mapping/MapProfile.cs
@@ -12,9 +12,7 @@ namespace HeaLEOO.Mapping
             ServiceModelMap();
             AppointmentMap();
             UserManagementMap();
-            CreateMap<RegisterVM,AppUser>()
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+            RegisterMap();
             CreateMap<AppUser, AppUserDto>();
             CreateMap<AppUserDto, AppUser>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
--- HeaLEOO/Mapping/MethodsMap/RegisterMap.cs
+++ HeaLEOO/Mapping/MethodsMap/RegisterMap.cs
@@ -2,5 +2,11 @@
 {
     public partial class MapProfile
     {
+        public void RegisterMap()
+        {
+            CreateMap<RegisterVM, AppUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+        }
     }
 }
