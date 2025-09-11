92b74cc88ecccd344316a2213dba727bb8d1929c
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-11 19:14:46 +0300

    Add Map Of Doctor

--- HeaLEOO/Mapping/MapProfile.cs
+++ HeaLEOO/Mapping/MapProfile.cs
@@ -6,17 +6,7 @@ namespace HeaLEOO.Mapping
     {
         public MapProfile()
         {
-            // doc
-           CreateMap<Doctors, DoctorViewModel>()
-          .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.specializations.Name))
-          .ForMember(dest => dest.SpecializationId, opt => opt.MapFrom(src => src.specializationId))
-          .ForMember(dest => dest.ClinicIds, opt => opt.MapFrom(src => src.ClinicDoctors.Select(cd => cd.ClinicId)))
-          .ForMember(dest => dest.ClinicNames, opt => opt.MapFrom(src => src.ClinicDoctors.Select(cd => cd.Clinic.Name)))
-          .ForMember(dest => dest.Specializations, opt => opt.Ignore())
-          .ForMember(dest => dest.Clinics, opt => opt.Ignore());
-           CreateMap<DoctorViewModel, Doctors>()
-           .ForMember(dest => dest.ClinicDoctors, opt => opt.Ignore())
-           .ForMember(dest => dest.specializations, opt => opt.Ignore());
+            DoctorMap();
             CreateMap<SpecializationsVM, Specializations>();
             CreateMap<Specializations, SpecializationsVM>();
             CreateMap<Clinics, ClinicVM>()
