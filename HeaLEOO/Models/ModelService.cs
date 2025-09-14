44b5582f3044e6eca6d8ebcb5cf8fbc9c9fb72ee
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-14 23:36:52 +0300

    ModelService EDIT

--- HeaLEOO/Models/ModelService.cs
+++ HeaLEOO/Models/ModelService.cs
@@ -2,5 +2,11 @@
 {
     public class ModelService
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public double Price { get; set; }
+        [ForeignKey("Clinic")]
+        public int ClinicId { get; set; }
+
     }
 }
