89b339c7c62ab47e889efe1ca2b969a7307bb1e6
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-14 23:06:58 +0300

    App Model Edit

--- HeaLEOO/Models/Appointments.cs
+++ HeaLEOO/Models/Appointments.cs
@@ -6,6 +6,15 @@
         public DateTime App_Date { get; set; }
         public string? status { get; set; }
         public bool? isActive { get; set; }
+        [ForeignKey("Doctors")]
+        public int DoctorId { get; set; }
+        [ForeignKey("Clinics")]
+        public int ClinicId { get; set; }
+        public Doctors Doctors { get; set; } = default!;
+        public Clinics Clinics { get; set; } = default!;
+        [ForeignKey("AppUser")]
+        public string AppUserId { get; set; } = default!;
+        public AppUser AppUser { get; set; } = default!;
 
     }
 }
