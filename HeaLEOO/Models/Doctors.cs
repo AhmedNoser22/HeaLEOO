5d31a7ca3ad93990f81832ab650105c2de627c3d
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-14 23:33:37 +0300

    Doctors Edit

--- HeaLEOO/Models/Doctors.cs
+++ HeaLEOO/Models/Doctors.cs
@@ -2,5 +2,10 @@
 {
     public class Doctors
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string phoneNumber { get; set; } = default!;
+        public int specializationId { get; set; }
+
     }
 }
