c8785a9dcb59cddd934c59eec9d365ab374e1a6a
mohamed22-m-m-e <modyshabara6@gmail.com>
2025-09-14 23:05:49 +0300

    Appointments Model Edit

--- HeaLEOO/Models/Appointments.cs
+++ HeaLEOO/Models/Appointments.cs
@@ -2,5 +2,10 @@
 {
     public class Appointments
     {
+        public int Id { get; set; }
+        public DateTime App_Date { get; set; }
+        public string? status { get; set; }
+        public bool? isActive { get; set; }
+
     }
 }
