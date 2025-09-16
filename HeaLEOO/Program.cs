1401ec88ca880f1a37b016d66c6498d3b7306244
Ahmed Noser <ahmednoser2222@gmail.com>
2025-09-16 07:36:54 +0300

    New Logic

--- HeaLEOO/Program.cs
+++ HeaLEOO/Program.cs
@@ -5,7 +5,6 @@ namespace HeaLEOO
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            //comment....
             // Add services to the container.
             builder.Services.AddHeaLEOOApp(builder.Configuration);
             builder.Services.AddControllersWithViews();
